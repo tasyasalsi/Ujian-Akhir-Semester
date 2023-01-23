457c6bac5f9c6c8e51aea9da8c1dbfb9a2582941
salsyii <114418450+tasyasalsi@users.noreply.github.com>
2023-01-23 14:49:48 +0700

    Update Jawaban2.py

--- Jawaban2.py
+++ Jawaban2.py
@@ -2,7 +2,7 @@ def convert(s):
     '''convert to integer'''
     try:
         x = int(s)
-        print("Konversi berhasil!, x=", x)
+        print("Konversi berhasil!, x = ", x)
     except ValueError:
         print("Konversi gagal!")
         x = -1
