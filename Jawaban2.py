c6d6c11e27714566adc031c513cb7d02c644d0ba
salsyii <114418450+tasyasalsi@users.noreply.github.com>
2023-01-23 14:57:12 +0700

    Update Jawaban2.py

--- Jawaban2.py
+++ Jawaban2.py
@@ -1,6 +1,6 @@
 try:
-    a = int(input('masukkan nilai a '))
-    b = int(input('masukkan nilai b '))
-    print('hasil a/b adalah',a/b)
+    a = int(input('masukkan nilai a: '))
+    b = int(input('masukkan nilai b: '))
+    print('hasil a/b adalah: ',a/b)
 except ZeroDivisionError:
     print('masukkan angka selain 0')
