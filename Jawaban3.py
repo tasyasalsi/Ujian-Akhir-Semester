c2ee9b284f0854686d56f2e99e2b7f9bf4f68ef1
salsyii <114418450+tasyasalsi@users.noreply.github.com>
2023-01-23 15:02:15 +0700

    Create Jawaban3.py

---
+++ Jawaban3.py
@@ -0,0 +1,9 @@
+import tkinter
+from tkinter import *
+root = Tk()
+root.wm_title("Jendela Utama")
+root.wm_iconbitmap('one.ico')
+root.geometry('300x200')
+root.configure(bg="white")
+root.state("normal")
+root.mainloop()
