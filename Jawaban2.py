0a347e8856a02691d593fc5d235ccf4600e8e1a3
salsyii <114418450+tasyasalsi@users.noreply.github.com>
2023-01-23 14:48:02 +0700

    Update Jawaban2.py

--- Jawaban2.py
+++ Jawaban2.py
@@ -1,1 +1,9 @@
-test
+def convert(s):
+    '''convert to integer'''
+    try:
+        x = int(s)
+        print("Konversi berhasil!, x=", x)
+    except ValueError:
+        print("Konversi gagal!")
+        x = -1
+    return x
